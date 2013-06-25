e6ecd69bc7cfda4a2430303e728f47be34e1e7bb
Ben Ng <me@benng.me>
2013-06-25 00:38:18 -0700

    Manually insert the grunt-qunit-phantomjs bridge

--- _test_sources/scripts/index.js
+++ _test_sources/scripts/index.js
@@ -5,12 +5,59 @@
   , $ = require('jquery-browserify')
   , App = require('../../_shared_sources/js/app/app.js')
   
+  //This is the grunt bridge
+  
+  // Don't re-order tests.
+  Q.config.reorder = false;
+  // Run tests serially, not in parallel.
+  Q.config.autorun = false;
+
+  // Send messages to the parent PhantomJS process via alert! Good times!!
+  function sendMessage() {
+    var args = [].slice.call(arguments);
+    alert(JSON.stringify(args));
+  }
+
+  // These methods connect Q to PhantomJS.
+  Q.log(function(obj) {
+    // What is this I don’t even
+    if (obj.message === '[object Object], undefined:undefined') { return; }
+    // Parse some stuff before sending it.
+    var actual = Q.jsDump.parse(obj.actual);
+    var expected = Q.jsDump.parse(obj.expected);
+    // Send it.
+    sendMessage('qunit.log', obj.result, actual, expected, obj.message, obj.source);
+  });
+
+  Q.testStart(function(obj) {
+    sendMessage('qunit.testStart', obj.name);
+  });
+
+  Q.testDone(function(obj) {
+    sendMessage('qunit.testDone', obj.name, obj.failed, obj.passed, obj.total);
+  });
+
+  Q.moduleStart(function(obj) {
+    sendMessage('qunit.moduleStart', obj.name);
+  });
+
+  Q.moduleDone(function(obj) {
+    sendMessage('qunit.moduleDone', obj.name, obj.failed, obj.passed, obj.total);
+  });
+
+  Q.begin(function() {
+    sendMessage('qunit.begin');
+  });
+
+  Q.done(function(obj) {
+    sendMessage('qunit.done', obj.failed, obj.passed, obj.total, obj.runtime);
+  });
+  
   /*
   * Testing config options
   */
   , previewSize = 70 //% of screen the preview should occupy
   , delay = 500;    //milliseconds between each test?
-  Q.config.reorder = false; //Don't reorder tests
   
   Backbone.$ = $;
   
