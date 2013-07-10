ecd0d7f1a48060e6a1cf849c31094e1103afe414
Ben Ng <me@benng.me>
2013-07-10 15:44:58 -0700

    Development baseUrl should be on port 4000

--- _shared_sources/js/app/config/config.js
+++ _shared_sources/js/app/config/config.js
@@ -1,17 +1,17 @@
 (function () {
   var config = {};
-  
+
   // APP CONFIG
   if(process.env.CI || process.env.NODE_ENV === 'staging') {
     config.baseUrl = "http://staging.toolkitt.com";
   }
   else if(process.env.NODE_ENV === 'development') {
-    config.baseUrl = "http://localdev.toolkitt.com";
+    config.baseUrl = "http://localdev.toolkitt.com:4000";
   }
   else {
     config.baseUrl = 'http://toolkitt.com';
   }
-  
+
   config.videoExts = ['.mp4', '.m4v', '.f4v', '.webm', '.ogv','.flv','.mov'];
   config.imageExts = ['.gif', '.png', '.jpeg', '.jpg', '.bmp'];
   config.thumbnailDims = {width:340,height:192};
@@ -20,6 +20,6 @@
   config.thumberUrl = 'https://toolkitt.s3.amazonaws.com/thumber.html';
   config.videoPlayerId = "media_video";
   config.imageViewerId = "media_image";
-  
+
   module.exports = config;
 }());
